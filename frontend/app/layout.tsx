db57a4aa5d8ba42934e07c213312bb6434931e56
rasla <programmer.rasla@gmail.com>
2025-01-17 17:14:31 +0530

    initial front end commit

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+
+const openSans = Open_Sans({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Aprisio",
+  description: "Aprisio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${openSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
