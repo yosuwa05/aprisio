4e4fe9495b748726d9e09606ac916d60cf52fb0e
rasla <programmer.rasla@gmail.com>
2025-02-13 10:32:43 +0530

    added

--- frontend/src/components/community/top-community-bar.tsx
+++ frontend/src/components/community/top-community-bar.tsx
@@ -15,6 +15,7 @@ import {
 import {
   Sheet,
   SheetContent,
+  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
--- frontend/src/components/groups/eventcard.tsx
+++ frontend/src/components/groups/eventcard.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/hooks/components/ui/button";
+import { Button } from "../ui/button";
 
 export function EventCard() {
   return (
