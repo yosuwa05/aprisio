7f7c71f3a4cd614ca46a8e13a325992b4ab2d7a6
rasla <programmer.rasla@gmail.com>
2025-02-15 13:08:42 +0530

    added events on profile screen

---
+++ frontend/src/components/pages/personal-events-section.tsx
@@ -0,0 +1,95 @@
+import { _axios } from "@/lib/axios-instance";
+import { useGlobalAuthStore } from "@/stores/GlobalAuthStore";
+import { useIntersection } from "@mantine/hooks";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
+import GlobalLoader from "../globalloader";
+import { Skeleton } from "../ui/skeleton";
+import { PersonalEventCard } from "./personalEventCard";
+
+export function PersonalEventsSection({}) {
+  const user = useGlobalAuthStore((state) => state.user);
+  const router = useRouter();
+  const limit = 10;
+
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+  }: any = useInfiniteQuery<any>({
+    queryKey: ["group-events", user?.id],
+    queryFn: async ({ pageParam = 1 }) => {
+      const res = await _axios.get(
+        `/noauth/group/events/me?userId=${user?.id}&page=${pageParam}&limit=${limit}`
+      );
+      return res?.data;
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage: any, allPages: any) => {
+      const nextPage = allPages?.length + 1;
+      return lastPage?.events?.length === limit ? nextPage : undefined;
+    },
+  });
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { ref, entry } = useIntersection({
+    root: containerRef.current,
+    threshold: 1,
+  });
+
+  const hasevents = !isLoading && data?.pages?.[0]?.events?.length > 0;
+
+  useEffect(() => {
+    if (entry?.isIntersecting && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [entry?.isIntersecting, hasNextPage, isFetchingNextPage]);
+
+  return (
+    <div className="my-4">
+      <div className="mt-6 flex-col flex gap-4">
+        {isLoading ? (
+          <div className="flex flex-col gap-4">
+            <div className="grid grid-cols-3 gap-4">
+              <Skeleton className="w-full h-[100px]  min-w-[250px]"> </Skeleton>
+              <Skeleton className="w-full h-[100px]  min-w-[250px]"> </Skeleton>
+              <Skeleton className="w-full h-[100px]  min-w-[250px]"> </Skeleton>
+            </div>
+
+            <div className="grid grid-cols-4 gap-4 w-full">
+              <Skeleton className="col-span-1 h-[100px]  min-w-[250px]">
+                {" "}
+              </Skeleton>
+              <Skeleton className="col-span-3 h-[100px]  min-w-[250px]">
+                {" "}
+              </Skeleton>
+            </div>
+          </div>
+        ) : hasevents ? (
+          data?.pages?.flatMap((page: any) =>
+            page?.events?.map((event: any, index: number) => {
+              return <PersonalEventCard key={event?._id} event={event} />;
+            })
+          )
+        ) : (
+          <div className="flex flex-col justify-center items-center gap-4">
+            <p className="text-gray-500 text-xs font-semibold">
+              No Events created
+            </p>
+          </div>
+        )}
+        <div ref={ref} className="h-10"></div>
+      </div>
+      {isLoading || isFetchingNextPage ? (
+        <div className="flex justify-center items-center my-4">
+          <GlobalLoader />
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+}
---
+++ frontend/src/components/pages/personal-groups-card.tsx
@@ -0,0 +1,136 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { _axios } from "@/lib/axios-instance";
+import { formatDate } from "@/lib/utils";
+import { useGlobalAuthStore } from "@/stores/GlobalAuthStore";
+import personImage from "@img/assets/person.png";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+interface IGroupCard {
+  name: string;
+  createdAt: string;
+  userId: IAdmin;
+  group: any;
+  canJoin: boolean;
+  slug: string;
+  memberCount: number;
+  _id: string;
+}
+
+interface IAdmin {
+  name: string;
+}
+
+interface Props {
+  group: IGroupCard;
+}
+
+interface IGroup {
+  id: String;
+}
+
+export function PersonalGroupCard({ group }: Props) {
+  const user = useGlobalAuthStore((state) => state.user);
+
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (group: IGroup) => {
+      return await _axios.post("/group/join", {
+        groupId: group.id,
+      });
+    },
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey: ["groups" + user?.id] });
+
+      const previousGroups = queryClient.getQueryData(["groups" + user?.id]);
+
+      queryClient.setQueryData(["groups" + user?.id], (old: any) => {
+        return {
+          ...old,
+          pages: old.pages.map((page: any) => ({
+            ...page,
+            data: {
+              ...page.data,
+              groups: page.data.groups.map((g: any) =>
+                g._id === group._id ? { ...g, canJoin: false } : g
+              ),
+            },
+          })),
+        };
+      });
+
+      return { previousGroups };
+    },
+    onSuccess: (data) => {
+      if (data.data.ok) {
+        toast(data.data.message || "Joined group successfully");
+      } else {
+        toast.error(data.data.message || "Something went wrong");
+      }
+    },
+  });
+
+  return (
+    <div
+      className="p-2 w-full rounded-lg transition-all"
+      style={{
+        boxShadow: "0px 0px 10px -1px rgba(2, 80, 124, 0.25)",
+      }}
+    >
+      <div className="flex gap-2 w-full items-center justify-between  h-[70px]">
+        <div className="flex gap-4 items-center">
+          <Image
+            src={personImage}
+            alt="person"
+            width={50}
+            height={50}
+            className="rounded-lg"
+          />
+
+          <div className="flex flex-col gap-2">
+            <h2 className="font-semibold">{group.group.name}</h2>
+
+            <div className="flex gap-6 items-center justify-between">
+              <p className="text-[#043A53] text-xs font-medium">
+                {group.memberCount} Member
+              </p>
+              <p className="text-gray-500 text-xs font-medium">
+                Created {formatDate(group.createdAt)}
+              </p>
+              <p className="text-[#828485] text-xs font-medium">
+                Organized by{" "}
+                <span className="font-bold text-[#636566]">
+                  {group.userId.name}
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <Button
+          disabled={isPending}
+          className={`${
+            group.canJoin
+              ? "bg-[#F2F5F6] border-[#043A53]"
+              : "bg-[#FCF7EA] border-[#AF9654]"
+          } rounded-3xl border-[0.2px]  hover:bg-[#FCF7EA] text-black`}
+          onClick={() => {
+            if (group.canJoin) {
+              mutate({
+                id: group._id,
+              });
+            } else {
+              router.push(`/groups/${group.slug}`);
+            }
+          }}
+        >
+          {group.canJoin ? "Join" : "View"}
+        </Button>
+      </div>
+    </div>
+  );
+}
