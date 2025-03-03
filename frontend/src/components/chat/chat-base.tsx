b28e642c20e42c3d68feb88bb371c51ada3293c2
rasla <programmer.rasla@gmail.com>
2025-03-03 14:05:26 +0530

    fixed

--- frontend/src/components/chat/chat-base.tsx
+++ frontend/src/components/chat/chat-base.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import { useState } from "react";
 import { ChatContact } from "./chat-contacts";
 
 export function ChatBase() {
-  const [open, setOpen] = useState(false);
-  const [label, setLabel] = useState("Online");
-
   return (
-    <div className="min-h-[calc(100vh-350px)] flex flex-col  p-4 h-[calc(100vh-450px)] overflow-y-auto hide-scrollbar">
+    <div className="min-h-[calc(100vh-350px)] flex flex-col  p-4 h-[calc(100vh-450px)] overflow-y-auto hide-scrollbar rounded-xl">
       <ChatContact />
     </div>
   );
